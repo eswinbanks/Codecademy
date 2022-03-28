e97a6da35a5a117f020a42bd0d324db5c92934e3
Evan Swinbanks <34035288+eswinbanks@users.noreply.github.com>
2022-03-28 17:49:30 +0100

    Create Program.cs

---
+++ Console_Creatures/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Trying to make a spooky ghost
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| o |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~'");
+     }
+  }
+}
